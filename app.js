1da4e403b93a317713d6d0a00bb4157270722c12
AlTimofeevM <al.timofeev.m@yandex.ru>
2019-12-15 14:19:19 +0300

    delete useless  files

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
-const db = require('./controller/dbController')
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-db.createUser()
+mongoose.connect("mongodb://user:password123@ds059947.mlab.com:59947/azureapp", { useNewUrlParser: true });
+
+
+const userScheme = new Schema({
+    vkontakteId: String,
+    imgs : [String]
+})
+const User = mongoose.model("User", userScheme);
+User.create({vkontakteId : "Ну да ну да"}, function(err, doc){
+    mongoose.disconnect();
+
+    if(err) return console.log(err);
+});
+
--- controller/dbController.js
+++
@@ -1,5 +0,0 @@
-const UserModel = require('../model/UserModel')
-
-exports.createUser = function(){
-    return UserModel.create({vkontakteId : "Ну да ну да"})
-}
