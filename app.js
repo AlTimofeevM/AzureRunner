0131f52ae68cd2fba634fd03286f8ee0d064a4b3
AlTimofeevM <al.timofeev.m@yandex.ru>
2019-12-15 15:13:03 +0300

    check env vars

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const Tesseract = require( 'tesseract.js');
 
 mongoose.connect("mongodb://user:password123@ds059947.mlab.com:59947/azureapp", { useNewUrlParser: true });
-
+console.log(process.env.var1)
+console.log(process.env.var2)
 
 const userScheme = new Schema({
     vkontakteId: String,
     imgs : [String]
 })
 const User = mongoose.model("User", userScheme);
-User.create({vkontakteId : "Ну да ну да"}, function(err, doc){
+User.create({vkontakteId : process.env.var1}, function(err, doc){
     mongoose.disconnect();
 
     if(err) return console.log(err);
