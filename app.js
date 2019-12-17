73b0f9b844f0b5f543abb50596110dc8fc8ad8bf
AlTimofeevM <al.timofeev.m@yandex.ru>
2019-12-17 04:12:54 +0300

    change Runner

--- app.js
+++ app.js
@@ -1,28 +1,29 @@
+const sentiment = require('multilang-sentiment');
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const Tesseract = require('tesseract.js');
 
 mongoose.connect("mongodb://user:password123@ds059947.mlab.com:59947/azureapp", { useNewUrlParser: true, useUnifiedTopology: true });
 
 const userSchema = mongoose.Schema({
     vkontakteId: String,
-    available: Number,
-    link: String,
-    text: String
+    emotion: String
 })
 
 const User = mongoose.model('User', userSchema)
 
-Tesseract.recognize(
-    process.env.LINK,
-    process.env.LANG,
-    { logger: m => console.log(m) }
-        ).then(async ({ data: { text } }) => {
-    await User.findOneAndUpdate({vkontakteId: process.env.ID}, {text : text} , function(err, doc){
-        mongoose.disconnect();
-    
-        if(err) return console.log(err);
-        })
-    }).catch((error) => {
-        console.log(error)
-    });
+
+let score = sentiment(process.env.TEXT, 'ru').score
+let emotion = "Нейтрально эмоциональное сообщение"
+if(score < 0){
+    emotion = "Негативное эмоциональное сообщение"
+}
+if(score > 0){
+    emotion = "Позитивно эмоциональное сообщение"
+}
+
+
+User.findOneAndUpdate({vkontakteId: process.env.ID}, { emotion :  emotion} , function(err, doc){
+    mongoose.disconnect();
+
+    if(err) return console.log(err);
+})
